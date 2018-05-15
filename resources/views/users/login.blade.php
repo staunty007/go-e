9f57a17e422379fa3ad6e6f638119fc9c1985c7b
Gabriel Adewumi <codergab@gmail.com>
2018-05-15 18:43:45 +0100

    Started Admin Backend

---
+++ resources/views/users/login.blade.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>GOENERGEE | Customer Main</title>
+
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/font-awesome/css/font-awesome.css" rel="stylesheet">
+	  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans:400,300'>
+	<link rel='stylesheet prefetch' href='https://fonts.googleapis.com/icon?family=Material+Icons'>
+    <link href="/css/animate.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+	<link href="/css/style1.css" rel="stylesheet">
+     
+	<link rel="icon" href="/img/favicon.png" type='image/x-icon'>
+
+</head>
+
+<body>
+   
+
+       <div class="container">
+	<div class="row">
+		<div class="col-sm-12 col-md-12 col-lg-12">
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+	<li data-target="#myCarousel" data-slide-to="4"></li>
+	
+	<li data-target="#myCarousel" data-slide-to="7"></li>
+	<li data-target="#myCarousel" data-slide-to="8"></li>
+	<li data-target="#myCarousel" data-slide-to="9"></li>
+	<li data-target="#myCarousel" data-slide-to="10"></li>
+	<li data-target="#myCarousel" data-slide-to="11"></li>
+  </ol>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="/img/1.png" class="img-responsive" style="height:300px;">
+    </div>
+
+    <div class="item">
+      <img src="/img/2.png" class="img-responsive" style="height:300px;">
+    </div>
+
+    <div class="item">
+      <img src="/img/3.png"  class="img-responsive" style="height:300px;">
+    </div>
+	  <div class="item">
+      <img src="/img/4.png" class="img-responsive" style="height:300px;">
+    </div>
+
+ 
+    <div class="item">
+      <img src="/img/7.png"  class="img-responsive" style="height:300px;">
+    </div>  <div class="item">
+      <img src="/img/8.png" class="img-responsive" style="height:300px;">
+    </div>
+
+    <div class="item">
+      <img src="/img/9.png"  class="img-responsive" style="height:300px;">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div></div>
+        
+            
+        
+<div class="wrapper wrapper-content animated fadeInRight">
+            <div class="row">
+                <div class="col-lg-7">
+                    <div class="ibox float-e-margins">
+                        
+                        <div class="ibox-content">
+                            <div>
+                                <div class="cotn_principal">
+<div class="cont_centrar">
+
+  <div class="cont_login">
+<div class="cont_info_log_sign_up">
+      <div class="col_md_login">
+<div class="cont_ba_opcitiy">
+        
+        <h2>LOGIN TO</h2> <img src="/img/logo.png">
+  <p>This is strictly for Administrators, Agents & DisCo.</p> 
+  <button class="btn_login" onclick="cambiar_login()">LOGIN</button>
+  </div>
+  </div>
+<div class="col_md_sign_up">
+<div class="cont_ba_opcitiy">
+  <h2>SIGN UP WITH</h2><img src="/img/logo.png">
+
+  
+  <p>Come onboard the leading Utility Payment Platform.</p>
+
+  <button class="btn_sign_up" onclick="cambiar_sign_up()">SIGN UP</button>
+</div>
+  </div>
+       </div>
+
+    
+    <div class="cont_back_info">
+       <div class="cont_img_back_grey">
+	   <img src="/img/transformers.jpg">
+       <!--<img src="https://images.unsplash.com/42/U7Fc1sy5SCUDIu4tlJY3_NY_by_PhilippHenzler_philmotion.de.jpg?ixlib=rb-0.3.5&q=50&fm=jpg&crop=entropy&s=7686972873678f32efaf2cd79671673d" alt="" />-->
+       </div>
+       
+    </div>
+<div class="cont_forms" >
+    <div class="cont_img_back_">
+	<img src="/img/bg.png">
+       <!--<img src="https://images.unsplash.com/42/U7Fc1sy5SCUDIu4tlJY3_NY_by_PhilippHenzler_philmotion.de.jpg?ixlib=rb-0.3.5&q=50&fm=jpg&crop=entropy&s=7686972873678f32efaf2cd79671673d" alt="" />-->
+       </div>
+ <div class="cont_form_login">
+<a href="#" onclick="ocultar_login_sign_up()" ><i class="material-icons">&#xE5C4;</i></a>
+   <h2>LOGIN TO</h2><img src="/img/logo.png">
+ <input type="text" placeholder="Email" />
+<input type="password" placeholder="Password" />
+<button class="btn_login" onclick="cambiar_login()">LOGIN</button>
+  </div>
+  
+   <div class="cont_form_sign_up">
+<a href="#" onclick="ocultar_login_sign_up()"><i class="material-icons">&#xE5C4;</i></a>
+     <h2>SIGN UP WITH</h2><img src="/img/logo.png">
+<input type="text" placeholder="Email" />
+<input type="password" placeholder="Password" />
+<input type="password" placeholder="Confirm Password" />
+<button class="btn_sign_up" onclick="cambiar_sign_up()">SIGN UP</button>
+
+  </div>
+
+    </div>
+    
+  </div>
+ </div>
+</div>
+  
+  
+
+    <script  src="js/index.js"></script>
+	
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+				<div class="col-lg-5">
+                    <div class="ibox float-e-margins">
+                        <div class="ibox-title">
+                            <h5>Information Portal</h5>
+                        </div>
+                        <div class="ibox-content ">
+                            <div class="carousel slide" id="carousel2">
+                                <ol class="carousel-indicators">
+                                    <li data-slide-to="0" data-target="#carousel2"  class="active"></li>
+                                    <li data-slide-to="1" data-target="#carousel2"></li>
+                                    <li data-slide-to="2" data-target="#carousel2" class=""></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="item active">
+                                        <img alt="image"  class="img-responsive" src="/img/p_big1.png">
+                                        
+                                    </div>
+                                    <div class="item ">
+                                        <img alt="image"  class="img-responsive" src="/img/p_big2.jpg">
+                                        
+                                    </div>
+                                    
+                                </div>
+                                <a data-slide="prev" href="#carousel2" class="left carousel-control">
+                                    <span class="icon-prev"></span>
+                                </a>
+                                <a data-slide="next" href="#carousel2" class="right carousel-control">
+                                    <span class="icon-next"></span>
+                                </a>
+                            </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+     
+              
+        </div>
+        <div class="footer">
+            
+            <div>
+                <strong>Powered by</strong> GOENERGEE &copy; 2018
+            </div>
+        </div>
+</div></div>
+    </div>
+      <!-- Mainly scripts -->
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="js/inspinia.js"></script>
+    <script src="js/plugins/pace/pace.min.js"></script>
+
+   
+
+    <!-- Mainly scripts -->
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+ 
+
+</body>
+</html>
