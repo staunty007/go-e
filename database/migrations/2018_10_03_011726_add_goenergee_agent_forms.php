7eeb0bd1c8e621331e3f6e92b7b9f52df70b1ea5
Gabriel Adewumi <codergab@gmail.com>
2018-10-03 02:17:54 +0100

    correcting migration on server

--- database/migrations/2018_10_03_011726_add_goenergee_agent_forms.php
+++ database/migrations/2018_10_03_011726_add_goenergee_agent_forms.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddAgentSignupDetailsToAgentsBiodata extends Migration
+class AddGoenergeeAgentForms extends Migration
 {
     /**
      * Run the migrations.
@@ -30,7 +30,7 @@ class AddAgentSignupDetailsToAgentsBiodata extends Migration
      */
     public function down()
     {
-        Schema::table('agents_biodata', function (Blueprint $table) {
+        Schema::table('agent_biodatas', function (Blueprint $table) {
             //
         });
     }
