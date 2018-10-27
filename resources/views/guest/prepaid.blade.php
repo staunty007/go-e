4bf36c2023d424ff9540448866cf9b6cf8a4fd8d
Gabriel Adewumi <codergab@gmail.com>
2018-10-27 12:40:34 +0100

    Working Payment Options

--- resources/views/guest/prepaid.blade.php
+++ resources/views/guest/prepaid.blade.php
@@ -349,13 +349,77 @@
 							{{-- </p> --}}
 							</div>
 						</div>
+					</form>
 				</div>
-				</form>
 			</div>
 		</div>
 	</div><!-- /.modal-content -->
-	</div><!-- /.modal-dialog -->
-	</div><!-- /.modal -->
+	<div class="modal fade" tabindex="-1" role="dialog" style="" id="payment-options">
+		<div class="modal-dialog " role="document">
+			<div class="modal-content">
+				<div class="modal-header headermodal text-center">
+					<button onclick="prevModalPaymentOptions()" class="btn btn-danger pull-right">Back</button>
+				</div>
+				<div id="payment-modal">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-5 col-md-5 col-sm-8 col-xs-9 bhoechie-tab-container">
+								<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 bhoechie-tab-menu">
+									<div class="list-group">
+										<a href="#" class="list-group-item active text-center">
+											<h4 class="fa fa-credit-card"></h4><br />Card Payment
+										</a>
+										<a href="#" class="list-group-item text-center">
+											<h4 class="fa fa-university"></h4><br />Bank to Bank
+										</a>
+										{{-- <a href="#" class="list-group-item text-center">
+											<h4 class="glyphicon glyphicon-road"></h4><br />Train
+										</a>
+										<a href="#" class="list-group-item text-center">
+											<h4 class="glyphicon glyphicon-home"></h4><br />Hotel
+										</a>
+										<a href="#" class="list-group-item text-center">
+											<h4 class="glyphicon glyphicon-cutlery"></h4><br />Restaurant
+										</a> --}}
+									</div>
+								</div>
+								<div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 bhoechie-tab">
+									<!-- card section -->
+									<div class="bhoechie-tab-content active">
+										<center>
+											<button class="btn btn-primary btn-lg" onclick="payWithPaystack();">Continue to Card Payment</button>
+										</center>
+									</div>
+									<!-- bank section -->
+									<div class="bhoechie-tab-content">
+										<center>
+											<div class="form-group">
+												<label>Please Select Your Bank</label>
+												<select class="form-control" name="bank_payment" id="bank_payment">
+													<option value="">Select your bank</option>
+													<option value="Diamond">Diamond Bank</option>
+													<option value="Zenith">Zenith Bank</option>
+												</select>
+											</div>
+											<div class="other">
+												<div class="form-group">
+													<label>Please Enter Your Account Number</label>
+													<input type="text" class="form-control" id="nuban" name="account">
+												</div>
+												<div class="form-group">
+													<button class="btn btn-block btn-primary" id="make-btn" disabled>Make Payment</button>
+												</div>
+											</div>
+										</center>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div><!-- /.modal-content -->
 
 
 
@@ -479,7 +543,7 @@
 								$("#total").val(parseInt($(".meter-amount").val()) + 100);
 								setTimeout(() => {
 									$('.pay-meter').html('Validated');
-									toggleMod();
+									confirmDetails();
 								}, 2000);
 							}
 						})
@@ -542,7 +606,7 @@
 				success: (response) => {
 					if (response.code == "ok") {
 						console.log(response.text);
-						payWithPaystack();
+						openOptions();
 					}
 				},
 				error: (err) => {
@@ -584,10 +648,21 @@
 			});
 			handler.openIframe();
 		}
-
-		function toggleMod() {
+		
+		// confirm payment details
+		const confirmDetails = () => {
 			$('#confirm-payment').modal('toggle');
 		}
+		// payment options
+		const openOptions = () => {
+			$(".modal").modal('hide');
+			$("#payment-options").modal('toggle');
+		}
+		const prevModalPaymentOptions = () => {
+			$('.modal').hide();
+			confirmDetails();
+		}
+
 	</script>
 	<script>
 		$(document).ready(function() {
@@ -604,10 +679,17 @@
 			$("#bank_payment").change(function() {
 				$(".other").css({
 					"transition":"all .2s cubic-bezier(0, 0.01, 0.35, 0.91)",
-					// "display":"block"
+					"display":"block"
 				});
 			});
+			
+			let nuban = document.querySelector("#nuban");
+			nuban.addEventListener('keyup', () => {
 
+				if(nuban.value.length !== 0) {
+					document.querySelector('#make-btn').disabled = false;
+				}
+			})
 			$("#payment-modal").iziModal('open');
 		});
 	</script>
