a41a9dc84432c18e406da9d535f3e17ece114597
Gabriel Adewumi <codergab@gmail.com>
2018-11-04 15:45:07 +0100

    Fixing Ussd Endpoint token

--- app/Http/Controllers/UssdController.php
+++ app/Http/Controllers/UssdController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\CIUssdController;
+use App\Http\Controllers\CIUssdController;
+
 
 class UssdController extends Controller
 {
-    public function register(Request $request)
+    public function register(Request $request, CIUssdController $ci)
     {
     	// Reads the variables sent via POST from our gateway
     	$sessionId   =$request->sessionId;
@@ -25,9 +26,9 @@ class UssdController extends Controller
 
             case '1':
                 // Create EKEDC Session
-                if(!session()->get('TAMSES')) {
-                    CIUssdController::signon();
-                }
+                // if(!session()->get('TAMSES')) {
+                    $ci->signon();
+                // }
 
                 $response = "CON Make Payment \n";
                 $response .= "1. Prepaid \n";
@@ -49,12 +50,12 @@ class UssdController extends Controller
                  * @param $customerId
                  * @return mixed
                  */
-                $result = CIUssdController::validateCustomer('PREPAID',$prepaid);
+                $result = $ci->validateCustomer('PREPAID',$prepaid);
                 if($result == true) {
                     $response = "CON Enter Amount you wish to pay";
                 }
                 // This is a terminal request. Note how we start the response with END
-                $response = "END ".$result;
+                $response = "CON ".$result;
             break;
 
             case '1*2':
