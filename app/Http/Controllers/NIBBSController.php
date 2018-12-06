5689a80b3d7802316b5a3b5dd5ce62006aa4f9f1
Gabriel Adewumi <codergab@gmail.com>
2018-12-06 10:59:34 +0100

    Nibbs

--- app/Http/Controllers/NIBBSController.php
+++ app/Http/Controllers/NIBBSController.php
@@ -7,39 +7,19 @@ use SoapClient;
 use SoapHeader;
 class NIBBSController extends Controller
 {
-    private $host = "http://staging.nibss-plc.com.ng/CentralPayWebservice/CentralPayOperations?wsdl";
+    private $host = "https://staging.nibss-plc.com.ng/CentralPayWebservice/CentralPayOperations?wsdl";
     // Get Banks List 
     public function getBanks()
     {
-        $contextOptions = array(
-            'ssl' => array(
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-            'allow_self_signed' => true
-            ));
-        
-        $sslContext = stream_context_create($contextOptions);
-        
-        $params =  array(
-            'trace' => 1,
-            'exceptions' => true,
-            'cache_wsdl' => WSDL_CACHE_NONE,
-            'stream_context' => $sslContext
-            );
         
         try {
-            $proxy = new SoapClient( $this->host, $params );
-        } catch (SoapFault $proxy) {
-            var_dump(libxml_get_last_error());
-            // var_dump($proxy);
+            
+            $client = new SoapClient($this->host);
+            // dd($client->listActiveBanks('NIBSS0000000128'));
+            dd($client->__getTypes());
+        } catch (\SoapFault $th) {
+            dd($th);
         }
-        // try {
-        //     libxml_disable_entity_loader(false);
-        //     $client = new \SoapClient($this->host, $params);
-        //     dd($client->listActiveBanks('NIBSS0000000128'));
-        // } catch (\SoapFault $th) {
-        //     dd($th);
-        // }
 
     }
 }
