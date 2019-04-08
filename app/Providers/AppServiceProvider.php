2f0deeb65c19edf0186390bdaa25ff7c501779f5
Gabriel Adewumi <codergab@gmail.com>
2019-04-08 13:48:07 +0100

    Added Production HTTPS force rule

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 use App\User;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
